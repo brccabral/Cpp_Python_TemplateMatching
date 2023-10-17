5faccc2a2fd76acdd1973b805d15055649620a53
Bruno Cabral <cabral.brc@gmail.com>
2023-10-17 12:03:33 -0700

    show farm

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ import numpy as np
 # %%
 farm_img = cv2.imread("assets/farm.png", cv2.IMREAD_UNCHANGED)
 needle_img = cv2.imread("assets/needle.png", cv2.IMREAD_UNCHANGED)
+
+# %%
+# show farm
+cv2.imshow("Farm", farm_img)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+# %%
