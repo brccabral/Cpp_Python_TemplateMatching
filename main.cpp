f1c16b5528213b7358e5b2a388a566ca34b23016
Bruno Cabral <cabral.brc@gmail.com>
2023-10-17 14:14:17 -0700

    show needle

--- main.cpp
+++ main.cpp
@@ -6,7 +6,13 @@ int main(int argc, char const *argv[])
     cv::Mat farm_img = cv::imread("assets/farm.png", cv::IMREAD_UNCHANGED);
     cv::Mat needle_img = cv::imread("assets/needle.png", cv::IMREAD_UNCHANGED);
 
+    // show farm
     cv::imshow("Farm", farm_img);
+    cv::waitKey();
+    cv::destroyAllWindows();
+
+    // show needle
+    cv::imshow("Needle", needle_img);
     cv::waitKey();
     cv::destroyAllWindows();
 
